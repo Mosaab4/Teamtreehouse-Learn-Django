3273b4aca66d5c30a20c2387495acbac4a842152
Mosaab Ehab <musab.ehab@gmail.com>
2017-06-19 01:01:03 +0200

    add course model test

--- Django Basics/learning_site/courses/tests.py
+++ Django Basics/learning_site/courses/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 
-from .models import Course
+from .models import Course ,Step
 
 class CourseModelTest(TestCase):
     def test_course_creation(self):
@@ -13,3 +13,18 @@ class CourseModelTest(TestCase):
         now = timezone.now()
 
         self.assertLess(course.created_at , now)
+
+class StepModelTest(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title = 'Python Testing',
+            description = 'Learn to write tests in python'
+        )
+
+    def test_step_creation(self):
+        step = Step.objects.create(
+            title='Introduction to Docsets',
+            description = 'Learn How to write tests in your docstrings',
+            course = self.course
+        )
+        self.assertIn(step , self.course.step_set.all())
